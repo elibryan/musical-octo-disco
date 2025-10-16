06a8e5b715fd8fb3bc194fd55ed3876216490346
Eli Holder <eli.bryan@gmail.com>
2025-10-16 12:13:07 -0400

    attempting package from github

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='3iaputilpy', version='0.1', packages = find_packages())
+setup(name='threeiaputilpy', version='0.1', packages = find_packages())
